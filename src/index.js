6d8d161b5219a8bda0e73f82dbf3dc71f2bbf7a0
Kevin Castejon <pitikev1@hotmail.com>
2019-10-11 22:05:02 +0200

    uppdate to fb

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@
 import 'dotenv/config';
 import '@babel/polyfill';
 
+const passport = require('passport');
+const FacebookStrategy = require('passport-facebook').Strategy;
+
 const { GraphQLServer } = require('graphql-yoga');
 const { prisma } = require('./generated/prisma-client');
 const Query = require('./resolvers/Query');
@@ -18,6 +21,15 @@ const resolvers = {
   User,
 };
 
+passport.use(new FacebookStrategy({
+  clientID: process.env.FB_API_ID,
+  clientSecret: process.env.FB_API_SECRET,
+  callbackURL: 'http://localhost:3000/auth/facebook/callback',
+},
+((accessToken, refreshToken, profile, cb) => {
+  console.log(profile.id);
+})));
+
 const server = new GraphQLServer({
   typeDefs: './src/schema.graphql',
   resolvers,
@@ -26,4 +38,15 @@ const server = new GraphQLServer({
     prisma,
   }),
 });
+
+server.get('/auth/facebook',
+  passport.authenticate('facebook'));
+
+server.get('/auth/facebook/callback',
+  passport.authenticate('facebook', { failureRedirect: '/login' }),
+  (req, res) => {
+    // Successful authentication, redirect home.
+    res.redirect('/');
+  });
+
 server.start(() => console.log('Server is running on http://localhost:4000'));
